6910054e6d3c4797ee38072050490a7b06a60d8e
Serge Guzik <guzik.sergey@gmail.com>
2016-03-09 09:16:31 +0600

    refactor: Remove redundant styles

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -167,12 +167,10 @@ public class ArticleDetailFragment extends Fragment implements
 
     private void setStatusBarColor(int color) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getActivity().getWindow();
-            // clear FLAG_TRANSLUCENT_STATUS flag:
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(color);
+            if (getActivity() != null) {
+                Window window = getActivity().getWindow();
+                window.setStatusBarColor(color);
+            }
         }
     }
 
